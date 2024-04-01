1b4d256e2967662f4f86668eaf714118e947302e
CAnonymous <zidong.yu@outlook.com>
2024-04-01 13:53:10 -0700

    Add a dummy get controller.

--- src/main/java/com/chuwa/tutorialtransaction/controller/OrderController.java
+++ src/main/java/com/chuwa/tutorialtransaction/controller/OrderController.java
@@ -3,11 +3,9 @@ package com.chuwa.tutorialtransaction.controller;
 import com.chuwa.tutorialtransaction.dto.OrderRequest;
 import com.chuwa.tutorialtransaction.dto.OrderResponse;
 import com.chuwa.tutorialtransaction.service.OrderService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -22,5 +20,10 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest){
         return ResponseEntity.ok(orderService.placeOrder(orderRequest));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponse> getOrder(@PathVariable(required = true) Long id) {
+        return ResponseEntity.ok(null);
     }
 }
